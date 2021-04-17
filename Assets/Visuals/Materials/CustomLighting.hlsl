33ca9bd10737d9ce36155a36a7768f0946cb1a56
linkouille <linkouillou@gmail.com>
2021-04-17 14:13:50 +0200

    Toon Shader

--- Assets/Visuals/Materials/CustomLighting.hlsl
+++ Assets/Visuals/Materials/CustomLighting.hlsl
@@ -3,7 +3,7 @@
 
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
 {
-#if defined(SHADERGRAPH_PREVIEW)
+#ifdef SHADERGRAPH_PREVIEW
     Direction = float3(0.5, 0.5, 0);
     Color = 1;
     DistanceAtten = 1;
@@ -25,7 +25,7 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
 
 void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
 {
-#if defined(SHADERGRAPH_PREVIEW)
+#ifdef SHADERGRAPH_PREVIEW
     Direction = half3(0.5, 0.5, 0);
     Color = 1;
     DistanceAtten = 1;
@@ -47,7 +47,7 @@ void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out h
 
 void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, float3 Color, float3 WorldNormal, float3 WorldView, out float3 Out)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Out = 0;
 #else
     Smoothness = exp2(10 * Smoothness + 1);
@@ -59,13 +59,13 @@ void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, f
 
 void DirectSpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Out = 0;
 #else
     Smoothness = exp2(10 * Smoothness + 1);
     WorldNormal = normalize(WorldNormal);
     WorldView = SafeNormalize(WorldView);
-    Out = LightingSpecular(Color, Direction, WorldNormal, WorldView,half4(Specular, 0), Smoothness);
+    Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, half4(Specular, 0), Smoothness);
 #endif
 }
 
